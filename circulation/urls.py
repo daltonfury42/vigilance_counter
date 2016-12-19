bfb118e41b8bd8e0cdac9fb5f5f8127f41253e26
daltonfury42 <daltonfury42@gmail.com>
2016-12-19 13:43:21 +0530

    Accepted rollNumber

--- circulation/urls.py
+++ circulation/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.views import generic
+from . import views
 
+app_name = 'circulation'
 urlpatterns = [
 	url(r'^$', generic.TemplateView.as_view(template_name='circulation/index.html')),
+	url(r'^acceptRoll', views.AcceptRollView, name='acceptRoll'),
+	url(r'^issue', views.IssueView, name='issue'),
 		]
--- circulation/views.py
+++ circulation/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+def IssueView(request):
+	if 'books' not in request.session.keys():
+		request.session['books'] = []
+	if 'accn_no' in request.POST.keys():
+		request.session['books'].append(request.POST['accn_no'])
+
+	return render(request, 'circulation/issue.html', { 'roll': request.session['roll'], 'books':request.session['books']})
 	
+	
+def AcceptRollView(request):
+	request.session['roll'] = request.POST['roll']
+
+	return HttpResponseRedirect(reverse('circulation:issue'))
