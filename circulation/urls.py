d27cf207cb4277ac43aa2cdf9a807297b89404ef
daltonfury42 <daltonfury42@gmail.com>
2016-12-30 23:08:11 +0530

    Added 'About' page

--- circulation/urls.py
+++ circulation/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^acceptRoll', views.AcceptRollView, name='acceptRoll'),
 	url(r'^cancelBook', views.CancelBookView, name='cancelBook'),
 	url(r'^issue', views.IssueView, name='issue'),
+	url(r'^about', generic.TemplateView.as_view(template_name='circulation/about.html'), name='about'),
 	url(r'^writeDB', views.WriteDBView, name='writeDB'),
 		]
