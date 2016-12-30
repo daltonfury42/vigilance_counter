d2b9e5bffe1b25df8aa7a788f1ec5ad1ccac1d08
daltonfury42 <daltonfury42@gmail.com>
2016-12-30 22:26:07 +0530

    Rewrote getQuery in a more elegant way

--- search/views.py
+++ search/views.py
@@ -1,37 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 from circulation.models import CheckOuts
 import csv
+import datetime
 
 # Create your views here.
 def getQueryView(request):
+	query_set = CheckOuts.objects.all()
 	if request.POST['rollno']:
-		roll = "'" + request.POST['rollno'] + "'"
-	else:
-		roll = 'roll'
+		query_set = query_set.filter(roll = request.POST['rollno'])
 
 	if request.POST['bookno']:
-		accn_no = "'" + request.POST['bookno'] + "'"
-	else:
-		accn_no = 'accn_no'
+		query_set = query_set.filter(accn_no = request.POST['bookno'])
 
 	if request.POST['from_dd'] and request.POST['from_mm'] and request.POST['from_yy']:
-		from_date = request.POST['from_yy'] + '-' + request.POST['from_mm'] + '-' + request.POST["from_dd"]
-	else:
-		from_date = '2016-01-01'
+		query_set = query_set.filter(time_stamp__gte = datetime.date(int(request.POST['from_yy']), int(request.POST['from_mm']), int(request.POST["from_dd"])))
 
 	if request.POST['to_dd'] and request.POST['to_mm'] and request.POST['to_yy']:
-		to_date = request.POST['to_yy'] + '-' + request.POST['to_mm'] + '-' + request.POST["to_dd"]
-	else:
-		to_date = '2032-01-01'
-
-	query = "SELECT * FROM circulation_checkouts WHERE roll = " + roll + " and accn_no = " + accn_no + " and time_stamp > Datetime('" + from_date + " 00:00:00') and time_stamp < Datetime('" + to_date + " 23:59:59')"
-	request.session['query'] = query
-	request.session.modified = True
+		query_set = query_set.filter(time_stamp__lte = datetime.date(int(request.POST['to_yy']), int(request.POST['to_mm']), int(request.POST["to_dd"])))
 	
-	results = CheckOuts.objects.raw(query)
+	request.session['query'] = str(query_set.query)
+	request.session.modified = True
 
-	return render(request, 'search/results.html', { 'results':results })
+	return render(request, 'search/results.html', { 
+		'results':query_set, 
+		'rollno':request.POST['rollno'], 
+		'bookno':request.POST['bookno'], 
+		'from_dd':request.POST['from_dd'], 
+		'from_mm':request.POST['from_mm'], 
+		'from_yy':request.POST['from_yy'],
+		'to_dd':request.POST['to_dd'], 
+		'to_mm':request.POST['to_mm'], 
+		'to_yy':request.POST['to_yy'],
+		
+		})
 		
 def downloadCsvView(request):
 	response = HttpResponse(content_type='text/csv')
@@ -40,8 +43,20 @@ def downloadCsvView(request):
 	writer = csv.writer(response)
 	writer.writerow(['Entry ID','Patron ID', 'Accession Number', 'Time Stamp'])
 
-	results = CheckOuts.objects.raw(request.session['query'])
-	for row in results:
+	query_set = CheckOuts.objects.all()
+	if 'rollno' in request.POST.keys():
+		query_set = query_set.filter(roll = request.POST['rollno'])
+
+	if 'bookno' in request.POST.keys():
+		query_set = query_set.filter(accn_no = request.POST['bookno'])
+
+	if request.POST['from_dd'] and request.POST['from_mm'] and request.POST['from_yy']:
+		query_set = query_set.filter(time_stamp__gte = datetime.date(int(request.POST['from_yy']), int(request.POST['from_mm']), int(request.POST["from_dd"])))
+
+	if request.POST['to_dd'] and request.POST['to_mm'] and request.POST['to_yy']:
+		query_set = query_set.filter(time_stamp__lte = datetime.date(int(request.POST['to_yy']), int(request.POST['to_mm']), int(request.POST["to_dd"])))
+	
+	for row in query_set:
 		writer.writerow([row.pk ,row.roll, row.accn_no, row.time_stamp])
 
 	return response
